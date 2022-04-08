33a75c378d205b323fba41c181c1c05b757b44f0
David Behar Lombrozo <david.behar@improving.com>
2022-04-07 21:29:30 -0500

    change name of library

--- src/types/viewer.types.ts
+++ src/types/viewer.types.ts
@@ -1,9 +1,8 @@
+// @ts-ignore
+import { Viewer, WebIFCLoaderPlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
 export interface ViewerConfig {
   canvasId: string;
   transparent?: boolean;
 }
 
-// @ts-ignore
-import { Viewer, WebIFCLoaderPlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
-
-export {WebIFCLoaderPlugin, Viewer}
+export { WebIFCLoaderPlugin, Viewer }
